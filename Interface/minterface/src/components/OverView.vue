b982df85a4947d833a78ca2a1e18845cc560f189
mjasim <mahmood.jasim.bd@gmail.com>
2020-03-12 15:11:02 -0400

    pre-build for vue-select

--- Interface/minterface/src/components/OverView.vue
+++ Interface/minterface/src/components/OverView.vue
@@ -1,84 +1,55 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+  <!-- <v-content> -->
+    <v-container fluid fill-height>
+      <v-layout justify-center>
+        <v-flex>
+          <template v-for="item in proposal_names">
+            <v-hover v-slot:default="{ hover }" open-delay="0" :key="item.id">
+              <v-card :elevation="hover ? 16 : 1" light style="margin: 5px">
+                <v-card-title flat style="padding: 2px">
+                  <div style="width: 20%">
+                    <v-layout id="proposals" row justify-start style="width: 15vw">
+                      <v-btn depressed text>{{ item.name }}</v-btn>
+                    </v-layout>
+                  </div>
+                  <div style="width: 40%">
+                    <v-layout id="topics" row justify-start>
+                      <template v-for="(topic, index) in item.topics">
+                        <v-btn depressed text :key="index">{{ topic }}</v-btn>
+                      </template>
+                    </v-layout>
+                  </div>
+                  <!-- <div style="width: 10%">
+                    <v-layout id="topic-dd" row justify-centert>
+                        <v-btn depressed fab small=""></v-btn>
+                    </v-layout>
+                  </div> -->
+                  <div style="width: 30%">
+                    <v-layout id="heatmapbox" row justify-start>
+                      <template v-for="(i, index) in item.discourse">
+                        <v-tooltip bottom :key="index">
+                          <template v-slot:activator="{on}">
+                            <v-btn
+                              v-on="on"
+                              depressed
+                              tile
+                              style="margin-left:1px; margin-right:1px; height:50px; min-width: 50px; width:50px; padding: 0px;"
+                              :color="i.color"
+                            ></v-btn>
+                          </template>
+                          <span>{{ i.value }}</span>
+                        </v-tooltip>
+                      </template>
+                    </v-layout>
+                  </div>
+                </v-card-title>
+              </v-card>
+            </v-hover>
+          </template>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  <!-- </v-content> -->
 </template>
 
 <script>
@@ -86,7 +57,203 @@ export default {
   name: "OverView",
 
   data: () => ({
-    
+    proposal_names: [
+      {
+        id: "1",
+        name: "Proposal_1",
+        topics: ["Topic_1", "Topic_2", "Topic_3"],
+        discourse: [
+          {
+            value: 12,
+            color: "red"
+          },
+          {
+            value: 7,
+            color: "blue"
+          },
+          {
+            value: 5,
+            color: "green"
+          },
+          {
+            value: 9,
+            color: "yellow"
+          },
+          {
+            value: 7,
+            color: "black"
+          },
+          {
+            value: 3,
+            color: "purple"
+          },
+          {
+            value: 2,
+            color: "white"
+          },
+          {
+            value: 90,
+            color: "brown"
+          }
+        ]
+      },
+      {
+        id: "2",
+        name: "Proposal_2",
+        topics: ["Topic_1", "Topic_2"],
+        discourse: [
+          {
+            value: 12,
+            color: "red"
+          },
+          {
+            value: 7,
+            color: "blue"
+          },
+          {
+            value: 5,
+            color: "green"
+          },
+          {
+            value: 9,
+            color: "yellow"
+          },
+          {
+            value: 7,
+            color: "black"
+          },
+          {
+            value: 3,
+            color: "purple"
+          },
+          {
+            value: 2,
+            color: "white"
+          },
+          {
+            value: 90,
+            color: "brown"
+          }
+        ]
+      },
+      {
+        id: "3",
+        name: "Proposal_3",
+        topics: ["Topic_1", "Topic_2", "Topic_3"],
+        discourse: [
+          {
+            value: 12,
+            color: "red"
+          },
+          {
+            value: 7,
+            color: "blue"
+          },
+          {
+            value: 5,
+            color: "green"
+          },
+          {
+            value: 9,
+            color: "yellow"
+          },
+          {
+            value: 7,
+            color: "black"
+          },
+          {
+            value: 3,
+            color: "purple"
+          },
+          {
+            value: 2,
+            color: "white"
+          },
+          {
+            value: 90,
+            color: "brown"
+          }
+        ]
+      },
+      {
+        id: "4",
+        name: "Proposal_4",
+        topics: ["Topic_1"],
+        discourse: [
+          {
+            value: 12,
+            color: "red"
+          },
+          {
+            value: 7,
+            color: "blue"
+          },
+          {
+            value: 5,
+            color: "green"
+          },
+          {
+            value: 9,
+            color: "yellow"
+          },
+          {
+            value: 7,
+            color: "black"
+          },
+          {
+            value: 3,
+            color: "purple"
+          },
+          {
+            value: 2,
+            color: "white"
+          },
+          {
+            value: 90,
+            color: "brown"
+          }
+        ]
+      },
+      {
+        id: "5",
+        name: "Proposal_5",
+        topics: ["Topic_1", "Topic_2", "Topic_3"],
+        discourse: [
+          {
+            value: 12,
+            color: "red"
+          },
+          {
+            value: 7,
+            color: "blue"
+          },
+          {
+            value: 5,
+            color: "green"
+          },
+          {
+            value: 9,
+            color: "yellow"
+          },
+          {
+            value: 7,
+            color: "black"
+          },
+          {
+            value: 3,
+            color: "purple"
+          },
+          {
+            value: 2,
+            color: "white"
+          },
+          {
+            value: 90,
+            color: "brown"
+          }
+        ]
+      }
+    ]
   })
 };
 </script>
