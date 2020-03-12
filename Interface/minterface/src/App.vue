6a422441e456d7550d8e09fa4b458be62149b833
mjasim <mahmood.jasim.bd@gmail.com>
2020-03-12 13:22:39 -0400

    interface base added

---
+++ Interface/minterface/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar app color="#2c485b" dark>
+      <v-btn text @click="goToOverview">Matryona</v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <OverView />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import OverView from "./components/OverView";
+
+export default {
+  name: "App",
+
+  components: {
+    OverView
+  },
+
+  data: () => ({
+    //
+  }),
+
+  methods: {
+    goToOverview(){
+      console.log("Go to Overview")
+    }
+  }
+};
+</script>
---
+++ Interface/minterface/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <OverView msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import OverView from "@/components/OverView.vue";
+
+export default {
+  name: "Home",
+  components: {
+    OverView
+  }
+};
+</script>
